9af9ae5bd43a2840bb32eafdb09bf78b329077d1
Rachnerd <rachnerd@hotmail.com>
2021-03-16 21:34:16 +0100

    Wrap whole post impl in promise

--- src/http.ts
+++ src/http.ts
@@ -12,36 +12,36 @@ export function post<R = undefined>(
   dataStringOrBuffer: string | Buffer,
   headers: OutgoingHttpHeaders = {}
 ): Promise<Http2Response<R>> {
-  const client: ClientHttp2Session =
-    typeof urlOrClient === "string" ? connect(urlOrClient) : urlOrClient;
-
-  const buffer =
-    typeof dataStringOrBuffer === "string" ? Buffer.from(dataStringOrBuffer) : dataStringOrBuffer;
-
-  const request = client.request({
-    [constants.HTTP2_HEADER_SCHEME]: "https",
-    [constants.HTTP2_HEADER_METHOD]: constants.HTTP2_METHOD_POST,
-    [constants.HTTP2_HEADER_PATH]: path,
-    [constants.HTTP2_HEADER_CONTENT_LENGTH]: Buffer.byteLength(buffer),
-    ...headers,
-  });
-  request.setEncoding("utf8");
-
-  const chunks: string[] = [];
-  request.on("data", (chunk) => chunks.push(chunk));
-
-  let status: number;
-  let contentType: string;
-
-  request.on("response", (headers, flags) => {
-    status = parseInt(headers[constants.HTTP2_HEADER_STATUS] as string, 10);
-    contentType = headers[constants.HTTP2_HEADER_CONTENT_TYPE] as string;
-  });
-
-  request.write(buffer);
-
-  const isLocalClientSession = typeof urlOrClient === "string";
   return new Promise<Http2Response<R>>((resolve, reject) => {
+    const client: ClientHttp2Session =
+      typeof urlOrClient === "string" ? connect(urlOrClient) : urlOrClient;
+
+    const buffer =
+      typeof dataStringOrBuffer === "string" ? Buffer.from(dataStringOrBuffer) : dataStringOrBuffer;
+
+    const request = client.request({
+      [constants.HTTP2_HEADER_SCHEME]: "https",
+      [constants.HTTP2_HEADER_METHOD]: constants.HTTP2_METHOD_POST,
+      [constants.HTTP2_HEADER_PATH]: path,
+      [constants.HTTP2_HEADER_CONTENT_LENGTH]: Buffer.byteLength(buffer),
+      ...headers,
+    });
+    request.setEncoding("utf8");
+
+    const chunks: string[] = [];
+    request.on("data", (chunk) => chunks.push(chunk));
+
+    let status: number;
+    let contentType: string;
+
+    request.on("response", (headers, flags) => {
+      status = parseInt(headers[constants.HTTP2_HEADER_STATUS] as string, 10);
+      contentType = headers[constants.HTTP2_HEADER_CONTENT_TYPE] as string;
+    });
+
+    request.write(buffer);
+
+    const isLocalClientSession = typeof urlOrClient === "string";
     request.on("end", () => {
       const body = chunks.join();
       if (status === 200) {
