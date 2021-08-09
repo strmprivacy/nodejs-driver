aefaa22a579a7146bea8e836b7bbad8d58ae11f9
jankeesvanandel <jankeesvanandel@gmail.com>
2021-08-09 15:12:30 +0200

    feat: added serialization export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { Sender, SenderConfig } from "./sender";
 export { Receiver, ReceiverConfig } from "./receiver";
 export { StrmEvent, StrmEventMetadata } from "./models/event";
+export { EventSerializerProvider, EventSerializer } from "./serialization"
